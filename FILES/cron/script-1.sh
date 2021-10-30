5d31f591c34954105aa7ff8c9fbe4e7a27d2c7d8
nahal <subhan.nahal@youtap.id>
2021-10-31 01:49:09 +0700

    reformat code

--- FILES/cron/script-1.sh
+++ FILES/cron/script-1.sh
@@ -2,19 +2,18 @@
 
 today=$(date +%Y-%m-%d)
 
-while read expired
-do
-	account=$(echo $expired | cut -d: -f1)
-	id=$(echo $expired | grep -v nobody | cut -d: -f3)
-	exp=$(chage -l $account | grep "Account expires" | awk -F": " '{print $2}')
-	if [[ $id -ge 1000 ]]; then
-		read mon day year <<< $exp
-		exp_date="$(date -d "$mon $day $year" "+%Y-%m-%d")"
-		if [[ $exp_date < $today ]]; then
-			userdel $account
-		fi
-	fi
-done < /etc/passwd
+while read expired; do
+  account=$(echo $expired | cut -d: -f1)
+  id=$(echo $expired | grep -v nobody | cut -d: -f3)
+  exp=$(chage -l $account | grep "Account expires" | awk -F": " '{print $2}')
+  if [[ $id -ge 1000 ]]; then
+    read mon day year <<<$exp
+    exp_date="$(date -d "$mon $day $year" "+%Y-%m-%d")"
+    if [[ $exp_date < $today ]]; then
+      userdel $account
+    fi
+  fi
+done </etc/passwd
 
 # Block Torrent
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
--- FILES/menu/menu-xray.sh
+++ FILES/menu/menu-xray.sh
@@ -1,92 +1,92 @@
 #!/bin/bash
 
 function login-limit() {
-	clear
-	limit=$(cat /iriszz/script-3/limit)
-	echo -e ""
-	echo -e "Current Dropbear multi-login limit: $limit"
-	echo -e ""
-	read -p "New Dropbear multi-login limit: " new_limit
-	if ! [[ $new_limit =~ ^[0-9]+$ ]]; then
-		echo ""
-		echo "'$new_limit' is not a number."
-		echo ""
-		exit 1
-	fi
-	echo $new_limit > /iriszz/script-3/limit
-	service script-3 restart
-	clear
-	echo -e ""
-	echo -e "Dropbear multi-login limit has been changed from '$limit' to '$new_limit'."
-	echo -e ""
+  clear
+  limit=$(cat /iriszz/script-3/limit)
+  echo -e ""
+  echo -e "Current Dropbear multi-login limit: $limit"
+  echo -e ""
+  read -p "New Dropbear multi-login limit: " new_limit
+  if ! [[ $new_limit =~ ^[0-9]+$ ]]; then
+    echo ""
+    echo "'$new_limit' is not a number."
+    echo ""
+    exit 1
+  fi
+  echo $new_limit >/iriszz/script-3/limit
+  service script-3 restart
+  clear
+  echo -e ""
+  echo -e "Dropbear multi-login limit has been changed from '$limit' to '$new_limit'."
+  echo -e ""
 }
 
 function block-conn() {
-	clear
-	status=$(cat /iriszz/block-status)
-	if [ "$status" == on ]; then
-		echo -e "Do you want to allow all connections?"
-		echo -e "  [1] Yes"
-		echo -e "  [2] No"
-		echo -e ""
-		until [[ ${option} =~ ^[1-2]$ ]]; do
-			read -rp "Select an option [1-2]: " option
-		done
-		if [ $option -eq 1 ]; then
-			ufw allow 85 > /dev/null 2>&1
-			ufw allow 465 > /dev/null 2>&1
-			ufw allow 8080 > /dev/null 2>&1
-			ufw allow 1194 > /dev/null 2>&1
-			ufw allow 80 > /dev/null 2>&1
-			ufw allow 443 > /dev/null 2>&1
-			ufw allow 51820 > /dev/null 2>&1
-			ufw allow 7300 > /dev/null 2>&1
-			ufw allow 8000 > /dev/null 2>&1
-			ufw allow 3128 > /dev/null 2>&1
-			echo off > /iriszz/block-status
-		elif [ $option -eq 2 ]; then
-			clear
-			echo -e "Operation cancelled."
-			echo -e ""
-			exit 0
-		fi
-		clear
-		echo -e ""
-		echo -e "All connections to the VPS is allowed."
-		echo -e ""
-	elif [ "$status" == off ]; then
-		echo -e "Do you want to block all connections?"
-		echo -e "  [1] Yes"
-		echo -e "  [2] No"
-		echo -e ""
-		until [[ ${option} =~ ^[1-2]$ ]]; do
-			read -rp "Select an option [1-2]: " option
-		done
-		if [ $option -eq 1 ]; then
-			ufw delete allow 85 > /dev/null 2>&1
-			ufw delete allow 465 > /dev/null 2>&1
-			ufw delete allow 8080 > /dev/null 2>&1
-			ufw delete allow 1194 > /dev/null 2>&1
-			ufw delete allow 80 > /dev/null 2>&1
-			ufw delete allow 443 > /dev/null 2>&1
-			ufw delete allow 51820 > /dev/null 2>&1
-			ufw delete allow 7300 > /dev/null 2>&1
-			ufw delete allow 8000 > /dev/null 2>&1
-			ufw delete allow 3128 > /dev/null 2>&1
-			echo on > /iriszz/block-status
-		elif [ $option -eq 2 ]; then
-			clear
-			echo -e "Operation cancelled."
-			echo -e ""
-			exit 0
-		fi
-		clear
-		echo -e ""
-		echo -e "All connections to the VPS is blocked, except SSH (22)."
-		echo -e ""
-	else
-		echo -e "Error!"
-	fi
+  clear
+  status=$(cat /iriszz/block-status)
+  if [ "$status" == on ]; then
+    echo -e "Do you want to allow all connections?"
+    echo -e "  [1] Yes"
+    echo -e "  [2] No"
+    echo -e ""
+    until [[ ${option} =~ ^[1-2]$ ]]; do
+      read -rp "Select an option [1-2]: " option
+    done
+    if [ $option -eq 1 ]; then
+      ufw allow 85 >/dev/null 2>&1
+      ufw allow 465 >/dev/null 2>&1
+      ufw allow 8080 >/dev/null 2>&1
+      ufw allow 1194 >/dev/null 2>&1
+      ufw allow 80 >/dev/null 2>&1
+      ufw allow 443 >/dev/null 2>&1
+      ufw allow 51820 >/dev/null 2>&1
+      ufw allow 7300 >/dev/null 2>&1
+      ufw allow 8000 >/dev/null 2>&1
+      ufw allow 3128 >/dev/null 2>&1
+      echo off >/iriszz/block-status
+    elif [ $option -eq 2 ]; then
+      clear
+      echo -e "Operation cancelled."
+      echo -e ""
+      exit 0
+    fi
+    clear
+    echo -e ""
+    echo -e "All connections to the VPS is allowed."
+    echo -e ""
+  elif [ "$status" == off ]; then
+    echo -e "Do you want to block all connections?"
+    echo -e "  [1] Yes"
+    echo -e "  [2] No"
+    echo -e ""
+    until [[ ${option} =~ ^[1-2]$ ]]; do
+      read -rp "Select an option [1-2]: " option
+    done
+    if [ $option -eq 1 ]; then
+      ufw delete allow 85 >/dev/null 2>&1
+      ufw delete allow 465 >/dev/null 2>&1
+      ufw delete allow 8080 >/dev/null 2>&1
+      ufw delete allow 1194 >/dev/null 2>&1
+      ufw delete allow 80 >/dev/null 2>&1
+      ufw delete allow 443 >/dev/null 2>&1
+      ufw delete allow 51820 >/dev/null 2>&1
+      ufw delete allow 7300 >/dev/null 2>&1
+      ufw delete allow 8000 >/dev/null 2>&1
+      ufw delete allow 3128 >/dev/null 2>&1
+      echo on >/iriszz/block-status
+    elif [ $option -eq 2 ]; then
+      clear
+      echo -e "Operation cancelled."
+      echo -e ""
+      exit 0
+    fi
+    clear
+    echo -e ""
+    echo -e "All connections to the VPS is blocked, except SSH (22)."
+    echo -e ""
+  else
+    echo -e "Error!"
+  fi
 }
 
 limit=$(cat /iriszz/script-3/limit)
@@ -117,48 +117,48 @@ echo -e ""
 echo -e "  [9] Exit"
 echo -e ""
 until [[ ${option} =~ ^[1-9]$ ]]; do
-	read -rp "Select an option [1-9]: " option
+  read -rp "Select an option [1-9]: " option
 done
 
 case "${option}" in
-	1)
-		ssh-vpn-script
-		exit
-		;;
-	2)
-		xray-script
-		exit
-		;;
-	3)
-		wireguard-script
-		exit
-		;;
-	4)
-		clear
-		speedtest
-		echo -e ""
-		exit
-		;;
-	5)
-		clear
-		echo -e ""
-		wget -qO- wget.racing/nench.sh | bash
-		exit
-		;;
-	6)
-		login-limit
-		exit
-		;;
-	7)
-		block-conn
-		exit
-		;;
-	8)
-		script-info
-		exit
-		;;
-	9)
-		clear
-		exit 0
-		;;
+1)
+  ssh-vpn-script
+  exit
+  ;;
+2)
+  xray-script
+  exit
+  ;;
+3)
+  wireguard-script
+  exit
+  ;;
+4)
+  clear
+  speedtest
+  echo -e ""
+  exit
+  ;;
+5)
+  clear
+  echo -e ""
+  wget -qO- wget.racing/nench.sh | bash
+  exit
+  ;;
+6)
+  login-limit
+  exit
+  ;;
+7)
+  block-conn
+  exit
+  ;;
+8)
+  script-info
+  exit
+  ;;
+9)
+  clear
+  exit 0
+  ;;
 esac
